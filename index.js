0c82493ff481b2aea17ae9ee935142daa4163669
jesus167 <ogutierrespinosa@gmail.com>
2022-05-13 17:04:22 -0400

    commit "animacion y detalles"

--- index.js
+++ index.js
@@ -13,27 +13,7 @@ $(document).ready(function () {
   };
 
   $("#pokemon_uno_boton").click(function () {
-    $.fn.getPokemon($("#pokemon_uno"), 25);
-  });
-
-  $("#pokemon_dos_boton").click(function () {
-    $.fn.getPokemon($("#pokemon_dos"), 100);
-  });
-
-  $("#pokemon_tres_boton").click(function () {
-    $.fn.getPokemon($("#pokemon_tres"), 235);
-  });
-
-  $("#pokemon_cuatro_boton").click(function () {
-    $.fn.getPokemon($("#pokemon_cuatro"), 425);
-  });
-
-  $("#pokemon_cinco_boton").click(function () {
-    $.fn.getPokemon($("#pokemon_cinco"), 625);
-  });
-
-  $("#pokemon_seis_boton").click(function () {
-    $.fn.getPokemon($("#pokemon_seis"), 725);
+    $.fn.getPokemon($("#pokemon_uno"), 6);
   });
 });
 
